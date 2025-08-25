5bab30c1ab19e5ea6a4806ced65cf2d33b023ad2
salam-07 <abdussalamblue7@gmail.com>
2025-08-25 11:41:53 +0500

    Implement getMessages controller and update message routes

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,13 +1,35 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
         const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-        res.status(200).json({ filteredUsers });
+        res.status(200).json(filteredUsers);
     } catch (error) {
         console.log("Error in getUsersForSidebar controller", error);
         res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId },
+            ]
+        });
+
+        res.status(200).json(messages);
+
+    } catch (error) {
+        console.log("Error in getMessages Controller", error);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 };
