5b835fe5ec137f4c664c927bb3c6b227281f23b9
salam-07 <abdussalamblue7@gmail.com>
2025-08-25 12:19:37 +0500

    Create Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,26 @@
+import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <div className="text-red-500 text-8xl">Hello World</div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+
+      </Routes>
+    </div>
   );
 };
 
