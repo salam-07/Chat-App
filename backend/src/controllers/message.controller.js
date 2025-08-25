522319ddb24a0e1d11479aaf48e0559517124d8d
salam-07 <abdussalamblue7@gmail.com>
2025-08-25 11:50:02 +0500

    Add sendMessage controller and update message routes for sending messages

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -30,6 +31,37 @@ export const getMessages = async (req, res) => {
 
     } catch (error) {
         console.log("Error in getMessages Controller", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if (imageUrl) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+        // TODO: implement realtime functionality
+
+        res.status(201).json(newMessage);
+
+    } catch (error) {
+        console.log("Error in sendMessage controller", error);
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
