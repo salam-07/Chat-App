302f04d60e8ae206f1b21dd11efeb3842b6ab473
salam-07 <abdussalamblue7@gmail.com>
2025-08-26 20:17:04 +0500

    Prepare for prod

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,12 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -24,6 +27,13 @@ app.use(cors(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log("Server running on port", PORT);
