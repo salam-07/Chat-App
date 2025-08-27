55e73de7cdbc24614d2a0e50ea3400a510b33e23
salam-07 <abdussalamblue7@gmail.com>
2025-08-27 08:06:56 +0500

    UI tweaks

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,8 +9,8 @@ const HomePage = () => {
 
     return (
         <div className="h-screen bg-base-200">
-            <div className="flex items-center justify-center pt-20 px-4">
-                <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-8xl h-[calc(100vh-7rem)]">
+            <div className="flex items-center justify-center pt-20">
+                <div className="bg-base-100 rounded-lg shadow-cl w-full h-screen">
                     <div className="flex h-full rounded-lg overflow-hidden">
                         <Sidebar />
 
